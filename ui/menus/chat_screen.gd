ab1c7f45f8b4302c1795b34e189cda545c5f63f6
2mellofanclub <rcarlsson0110@gmail.com>
2026-03-27 21:14:43 +0200

    7.7: chat window display logic works (ish), http next

--- ui/menus/chat_screen.gd
+++ ui/menus/chat_screen.gd
@@ -1,7 +1,44 @@
 extends CanvasLayer
 
 
+@onready var chat_bubble = load("res://ui/menus/chat_bubble.tscn")
+@onready var scroll_container = %ScrollContainer
+@onready var chat_container = %ChatContainer
+@onready var input_field = %InputField
+
 
 func _ready() -> void:
 	SignalBus.chat_opened.connect(show)
+	SignalBus.chat_opened.connect(input_field.grab_focus)
 	SignalBus.chat_closed.connect(hide)
+
+
+func add_bubble(text: String, side: String):
+	var chat_bubble_instance = chat_bubble.instantiate()
+	chat_bubble_instance.get_node("BubbleText").text = text
+	if side == "left":
+		chat_bubble_instance.set_h_size_flags(Control.SizeFlags.SIZE_SHRINK_BEGIN)
+	else:
+		chat_bubble_instance.set_h_size_flags(Control.SizeFlags.SIZE_SHRINK_END)
+		add_debug_reply()
+	chat_container.add_child(chat_bubble_instance)
+	await get_tree().process_frame
+	scroll_container.ensure_control_visible(chat_bubble_instance)
+
+
+func add_debug_reply():
+	await get_tree().create_timer(1).timeout
+	var musketeerism = "\"Great criminals bear about them a kind of predestination which makes them surmount all obstacles, which makes them escape all dangers, up to the moment which a wearied Providence has marked as the rock of their impious fortunes.\""
+	add_bubble(musketeerism, "left")
+
+
+func submit_input(input_text):
+	input_field.set_text("")
+	add_bubble(input_text, "right")
+	
+	
+func _on_submit_button_pressed() -> void:
+	submit_input(input_field.get_text())
+	input_field.grab_focus()
+func _on_input_field_text_submitted(new_text: String) -> void:
+	submit_input(new_text)
