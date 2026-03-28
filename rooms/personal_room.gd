b6361fd70dfdb035d32750eb65e1e90f705ec3c6
2mellofanclub <rcarlsson0110@gmail.com>
2026-03-28 18:21:55 +0200

    8.2: added some color varations of walls/floors for testing

--- rooms/personal_room.gd
+++ rooms/personal_room.gd
@@ -8,7 +8,6 @@ var plot_marker : Marker2D
 
 
 # so here we'd have some code that
-# - launches room specific menus on ui_cancel, other inputs
 # - operates game logic / interacts with Game object
 # - keeps track of players in room
 # - does whatever else we need the room itself to do
