f53a5af0d4c9f13433c838bad5bea364dc0ab57a
2mellofanclub <rcarlsson0110@gmail.com>
2026-04-16 04:13:35 +0300

    10.61: messing with colors and stuff

--- rooms/rainbow_room.gd
+++ rooms/rainbow_room.gd
@@ -1,10 +1,3 @@
 extends Node2D
 
 var display_name = "Rainbow Room"
-
-
-func _process(_delta: float) -> void:
-	var player_locations : Array[Vector2]
-	for player in get_tree().get_nodes_in_group("players"):
-		player_locations.append(player.global_position)
-	$Floor.get_material().set_shader_parameter("player_locs", player_locations)
