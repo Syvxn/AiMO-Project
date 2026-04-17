04d6ca77ea96bb858ea2372c5e64f74cb8d8f940
2mellofanclub <rcarlsson0110@gmail.com>
2026-04-17 10:20:08 +0300

    10.71: updated .env_example.json

--- rooms/personal_room.gd
+++ rooms/personal_room.gd
@@ -3,11 +3,3 @@ extends Node2D
 var owner_username : String
 var owner_peer_id : int
 var plot_marker : Marker2D
-#var players_in_room = []
-
-
-
-# so here we'd have some code that
-# - operates game logic / interacts with Game object
-# - keeps track of players in room
-# - does whatever else we need the room itself to do
