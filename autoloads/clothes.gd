7d6921b32b0384eb689538c37ea841696e0ab411
2mellofanclub <rcarlsson0110@gmail.com>
2026-04-22 10:14:14 +0300

    11.1: ch-ch-ch-ch-changes, tuuurn and face the strange

--- autoloads/clothes.gd
+++ autoloads/clothes.gd
@@ -2,36 +2,36 @@ extends Node
 
 
 var body_items = {
-	"" : "res://textures/sprite_frames/items_mono/empty.tres",
-	"basic" : "res://textures/sprite_frames/items_mono/mono_body_basic.tres",
+	"" : {"name" : "Nothing", "path" : "res://textures/sprite_frames/items_mono/empty.tres"},
+	"basic" : {"name" : "Basic", "path" : "res://textures/sprite_frames/items_mono/mono_body_basic.tres"},
 }
 var eyes_items = {
-	"" : "res://textures/sprite_frames/items_mono/empty.tres",
-	"basic" : "res://textures/sprite_frames/items_mono/mono_eyes_basic.tres"
+	"" : {"name" : "Nothing", "path" : "res://textures/sprite_frames/items_mono/empty.tres"},
+	"basic" : {"name" : "Basic", "path" : "res://textures/sprite_frames/items_mono/mono_eyes_basic.tres"},
 }
 var hair_items = {
-	"" : "res://textures/sprite_frames/items_mono/empty.tres",
-	"faith" : "res://textures/sprite_frames/items_mono/mono_hair_faith.tres",
-	"chell" : "res://textures/sprite_frames/items_mono/mono_hair_chell.tres"
+	"" : {"name" : "Nothing", "path" : "res://textures/sprite_frames/items_mono/empty.tres"},
+	"faith" : {"name" : "Faith", "path" : "res://textures/sprite_frames/items_mono/mono_hair_faith.tres"},
+	"chell" : {"name" : "Chell", "path" : "res://textures/sprite_frames/items_mono/mono_hair_chell.tres"},
 }
 var shirt_items = {
-	"" : "res://textures/sprite_frames/items_mono/empty.tres",
-	"tanktop_faith" : "res://textures/sprite_frames/items_mono/mono_tanktop_faith.tres",
-	"tshirt_basic" : "res://textures/sprite_frames/items_mono/mono_tshirt_basic.tres",
-	"croptop_basic" : "res://textures/sprite_frames/items_mono/mono_croptop_basic.tres",
+	"" : {"name" : "Nothing", "path" : "res://textures/sprite_frames/items_mono/empty.tres"},
+	"tanktop_faith" : {"name" : "Tanktop (Faith)", "path" : "res://textures/sprite_frames/items_mono/mono_tanktop_faith.tres"},
+	"tshirt_basic" : {"name" : "T-shirt (Basic)", "path" : "res://textures/sprite_frames/items_mono/mono_tshirt_basic.tres"},
+	"croptop_basic" : {"name" : "Crop top (Basic)", "path" : "res://textures/sprite_frames/items_mono/mono_croptop_basic.tres"},
 }
 #var jacket_items = {}
 var pants_items = {
-	"" : "res://textures/sprite_frames/items_mono/empty.tres",
-	"pants_faith" : "res://textures/sprite_frames/items_mono/mono_pants_faith.tres",
+	"" : {"name" : "Nothing", "path" : "res://textures/sprite_frames/items_mono/empty.tres"},
+	"pants_faith" : {"name" : "Pants (Faith)", "path" : "res://textures/sprite_frames/items_mono/mono_pants_faith.tres"},
 }
 var shoes_items = {
-	"" : "res://textures/sprite_frames/items_mono/empty.tres",
-	"shoes_faith" : "res://textures/sprite_frames/items_mono/mono_shoes_faith.tres",
+	"" : {"name" : "Nothing", "path" : "res://textures/sprite_frames/items_mono/empty.tres"},
+	"shoes_faith" : {"name" : "Runners (Faith)", "path" : "res://textures/sprite_frames/items_mono/mono_shoes_faith.tres"},
 }
 var costumes = {
-	"" : "res://textures/sprite_frames/items_mono/empty.tres",
-	"chell" : "res://textures/sprite_frames/chell.tres",
-	"chell_var1" : "res://textures/sprite_frames/chell_var1.tres",
-	"faith" : "res://textures/sprite_frames/faith.tres",
+	"" : {"name" : "Nothing", "path" : "res://textures/sprite_frames/items_mono/empty.tres"},
+	"chell" : {"name" : "Chell", "path" : "res://textures/sprite_frames/chell.tres"},
+	"chell_var1" : {"name" : "Chell (Var1)", "path" : "res://textures/sprite_frames/chell_var1.tres"},
+	"faith" : {"name" : "Faith", "path" : "res://textures/sprite_frames/faith.tres"},
 }
