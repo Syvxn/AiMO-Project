5791a867f66c6fa1f0bddf1ccad76e4774f04ee2
2mellofanclub <rcarlsson0110@gmail.com>
2026-05-14 15:24:02 +0300

    starting work on minigame HUD

--- apps/game/ui/menus/chat_and_quiz/quiz_question.gd
+++ apps/game/ui/menus/chat_and_quiz/quiz_question.gd
@@ -22,15 +22,15 @@ func check_answer(show_color=true) -> bool:
 	var selected_answer_index = item_list.get_selected_items()[0] # should only be one
 	if item_list.get_item_text(selected_answer_index).begins_with(correct_key):
 		if show_color:
-			item_list.set_item_custom_bg_color(selected_answer_index, Color("FOREST_GREEN"))
+			item_list.set_item_custom_bg_color(selected_answer_index, Color("42a964ff"))
 		return true
 	else:
 		if show_color:
-			item_list.set_item_custom_bg_color(selected_answer_index, Color("DARK_RED"))
+			item_list.set_item_custom_bg_color(selected_answer_index, Color("cc2823ff"))
 		return false
 
 
 func show_correct_answer():
 	for i in range(item_list.item_count):
 		if item_list.get_item_text(i).begins_with(correct_key):
-			item_list.set_item_custom_bg_color(i, Color("FOREST_GREEN"))
+			item_list.set_item_custom_bg_color(i, Color("42a964ff"))
